cfd767ad8021da8b1d133dd4ce3ad5836e28effd
Programacion6 <programacion6@megabyteca.com>
2016-11-07 13:31:45 -0400

    py1: prime numbers

--- primes.py
+++ primes.py
@@ -1,16 +1,27 @@
 """
 Your module documentation here
 """
+import math
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Class to calculate whether a number is prime
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Method for calculating prime number
+
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int == 2:
+            return True
+        if num_int % 2 == 0 or num_int <= 1:
+            return False
+
+        sqr = int(math.sqrt(num_int)) + 1
+        for divisor in range(3, sqr, 2):
+            if num_int % divisor == 0:
+                return False
+        return True
